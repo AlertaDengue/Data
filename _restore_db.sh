cfea8cd92f8f65b45a89a1a7105f3ff385912baf
Sandro Loch <es.loch@gmail.com>
2019-05-28 23:19:45 -0300

    Update _restore_db.sh

--- _restore_db.sh
+++ _restore_db.sh
@@ -9,3 +9,5 @@ psql -d dengue < alertademo_data.sql
 
 #gzip -d infodengue.sql.gz
 #psql -d infodengue < infodengue.sql
+# Insert into denguedb table municipio:notificacao path/notificacao.sql
+psql -d dengue < path/notificacao.sql
