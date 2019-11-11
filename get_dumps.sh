81a6776679dc561de611e42353c82ca27b00f1b2
Sandro Loch <es.loch@gmail.com>
2019-11-10 21:18:12 -0300

    Inserted script wget dumps

---
+++ get_dumps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --user dev_dumps --password t7AR7Y226QUJuKoAv7DuxHdXnDEteboW -c https://info.dengue.mat.br/dev_dumps/dengue.sql.gz
+
+wget --user dev_dumps --password t7AR7Y226QUJuKoAv7DuxHdXnDEteboW -c https://info.dengue.mat.br/dev_dumps/infodengue.sql.gz
