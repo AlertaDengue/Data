815a3e28956fb6a676cec328dbdaa026def49635
Sandro Loch <es.loch@gmail..com>
2020-12-10 16:19:35 -0300

    Add sql files

--- docker/patch/notificacao.sql
+++ docker/patch/notificacao.sql
@@ -1,3 +1,42 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:99e1f8d3ae40381b1aca84ed3042ca487d8909472ac6a037d5997936628d8e14
-size 1105
+-- Table: "Municipio"."Notificacao"
+
+-- DROP TABLE "Municipio"."Notificacao";
+
+CREATE TABLE "Municipio"."Notificacao"
+(
+  id bigserial NOT NULL,
+  dt_notific date,
+  se_notif integer,
+  ano_notif integer,
+  dt_sin_pri date,
+  se_sin_pri integer,
+  dt_digita date,
+  bairro_nome text,
+  bairro_bairro_id integer,
+  municipio_geocodigo integer,
+  nu_notific integer,
+  cid10_codigo character varying(5),
+  dt_nasc date,
+  cs_sexo character varying(1),
+  nu_idade_n integer,
+  CONSTRAINT "Notificacao_pk" PRIMARY KEY (id),
+  CONSTRAINT casos_unicos UNIQUE (nu_notific, dt_notific, cid10_codigo, municipio_geocodigo)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Municipio"."Notificacao"
+  OWNER TO administrador;
+GRANT ALL ON TABLE "Municipio"."Notificacao" TO administrador;
+GRANT ALL ON TABLE "Municipio"."Notificacao" TO "Dengue";
+COMMENT ON TABLE "Municipio"."Notificacao"
+  IS 'Casos de notificacao de dengue';
+
+-- Index: "Municipio"."Dengue_idx_data"
+
+-- DROP INDEX "Municipio"."Dengue_idx_data";
+
+CREATE INDEX "Dengue_idx_data"
+  ON "Municipio"."Notificacao"
+  USING btree
+  (dt_notific DESC, se_notif DESC);
--- docker/patch/update_hist_alerta.sql
+++ docker/patch/update_hist_alerta.sql
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7ee21548c2c2aec9a9a1fafa3e5028f8b9f86062bd9597b3b4422e5e04d057be
-size 1323
+-- Tables:
+
+
+-- "Municipio"."Historico_alerta"
+ALTER TABLE "Municipio"."Historico_alerta" ADD COLUMN Rt numeric(5,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta" ADD COLUMN pop numeric(7,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta" ADD COLUMN tempmin numeric(4,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta" ADD COLUMN umidmax numeric(4,0) default NULL::numeric;
+-- "Municipio"."Historico_alerta_chik"
+ALTER TABLE "Municipio"."Historico_alerta_chik" ADD COLUMN Rt numeric(5,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta_chik" ADD COLUMN pop numeric(7,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta_chik" ADD COLUMN tempmin numeric(4,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta_chik" ADD COLUMN umidmax numeric(4,0) default NULL::numeric;
+-- "Municipio"."Historico_alerta_zika"
+ALTER TABLE "Municipio"."Historico_alerta_zika" ADD COLUMN Rt numeric(5,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta_zika" ADD COLUMN pop numeric(7,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta_zika" ADD COLUMN tempmin numeric(4,0) default NULL::numeric;
+ALTER TABLE "Municipio"."Historico_alerta_zika" ADD COLUMN umidmax numeric(4,0) default NULL::numeric;
